5e64a71e91205fa0ea956e2ca669c4c18e221c3a
Dico Diaz <dicodiaz@gmail.com>
2023-07-10 19:37:58 -0500

    Set up Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/__tests__/setup.ts'],
+    include: ['./src/__tests__/**/*.test.tsx'],
+    globals: true,
+  },
 });
